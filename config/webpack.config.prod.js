ace4361da1f25cb90b5203e6116ec0cca43b0d45
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-07-29 16:37:00 +0100

    fix hash classes bug

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -184,7 +184,7 @@ module.exports = {
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                         modules: true,
-                        localIdentName: '[name]__[local]__[hash:64:5]'
+                        localIdentName: '[name]__[local]__[hash:base64:5]'
                       },
                     },
                     {
